6fc20549a66336aed6edadfbeb2b052b7ad6ffc0
Pan Piotr <piotr.krasun@gmail.com>
2016-03-15 21:54:19 +0000

    Cleaned up unnecessary files.

--- file_util.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FILE_UTIL_H__
-#define __FILE_UTIL_H__
-
-
-#endif // __FILE_UTIL_H__
-
--- task_specific.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TASK_SPECIFIC_H__
-#define __TASK_SPECIFIC_H__
-
-#include "structs.h"
-
-
-
-#endif // __TASK_SPECIFIC_H__
-

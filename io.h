e5a59259c1eaf04aa1acc307867bef099283ac2c
piokra <piokra@users.noreply.github.com>
2016-03-30 18:16:48 +0200

    Update io.h

--- io.h
+++ io.h
@@ -4,6 +4,7 @@
 #include "structs.h"
 
 workload read_vectors_from_file(char* filename);
+workload read_vectors_from_file_parallel(char* filename);
 void log_worktimes(worktime* worktimes, int count);
 
 #endif // __IO_H__
